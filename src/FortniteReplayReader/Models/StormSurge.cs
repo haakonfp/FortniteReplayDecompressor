15a1196fa3bc50f65fb0ad9d9ac00324358572c9
Lucas Simon <luke5227@gmail.com>
2021-06-26 17:24:56 -0400

    Track storm surges

---
+++ src/FortniteReplayReader/Models/StormSurge.cs
@@ -0,0 +1,12 @@
+namespace FortniteReplayReader.Models
+{
+    // DamageTime/ClearTime may be 0 if replay ends early.
+    // DamageTime may be 0 if storm surge ends before
+    //     any damage is done.
+    public class StormSurge
+    {
+        public float WarningTime { get; internal set; }
+        public float DamageTime { get; internal set; }
+        public float ClearTime { get; internal set; }
+    }
+}
