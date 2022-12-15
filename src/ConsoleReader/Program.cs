713283cf8230847655e138fc8917f511ceee170f
Haakon Fuhre Pettersen <haakonsnexus@gmail.com>
2022-12-15 14:10:17 +0100

    Minor update to ConsoleReader

--- src/ConsoleReader/Program.cs
+++ src/ConsoleReader/Program.cs
@@ -9,62 +9,61 @@ using System.IO;
 using System.Linq;
 using Unreal.Core.Models.Enums;
 
-namespace ConsoleReader
+namespace ConsoleReader;
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        var serviceCollection = new ServiceCollection()
+            .AddLogging(loggingBuilder => loggingBuilder
+                .AddConsole()
+                .SetMinimumLevel(LogLevel.Warning));
+        var provider = serviceCollection.BuildServiceProvider();
+        var logger = provider.GetService<ILogger<Program>>();
+
+        Stopwatch sw = new();
+
+        double totalTime = 0;
+        int count = 0;
+
+        List<double> times = new();
+
+        var reader = new ReplayReader(null, new FortniteReplaySettings
         {
-            var serviceCollection = new ServiceCollection()
-                .AddLogging(loggingBuilder => loggingBuilder
-                    .AddConsole()
-                    .SetMinimumLevel(LogLevel.Warning));
-            var provider = serviceCollection.BuildServiceProvider();
-            var logger = provider.GetService<ILogger<Program>>();
+            PlayerLocationType = LocationTypes.None,
+        });
 
-            Stopwatch sw = new();
+        foreach (string replayFile in Directory.GetFiles(@"Replays\"))
+        {
+            Console.WriteLine(replayFile);
 
-            double totalTime = 0;
-            int count = 0;
-
-            List<double> times = new();
-
-            var reader = new ReplayReader(null, new FortniteReplaySettings
+            for (int i = 0; i < 2; i++)
             {
-                PlayerLocationType = LocationTypes.None,
-            });
+                ++count;
 
-            foreach (string replayFile in Directory.GetFiles(@"Replays\"))
-            {
-                Console.WriteLine(replayFile);
+                sw.Restart();
+                var replay = reader.ReadReplay(replayFile, ParseType.Full);
 
-                for (int i = 0; i < 2; i++)
-                {
-                    ++count;
+                sw.Stop();
 
-                    sw.Restart();
-                    var replay = reader.ReadReplay(replayFile, ParseType.Full);
+                var b = replay.GameInformation.Players.OrderBy(x => x.Placement);
 
-                    sw.Stop();
+                Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms. Total Groups Read: {reader?.TotalGroupsRead}. Failed Bunches: {reader?.TotalFailedBunches}. Failed Replicator: {reader?.TotalFailedReplicatorReceives} Null Exports: {reader?.NullHandles} Property Errors: {reader?.PropertyError} Failed Property Reads: {reader?.FailedToRead}");
 
-                    var b = replay.GameInformation.Players.OrderBy(x => x.Placement);
-
-                    Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms. Total Groups Read: {reader?.TotalGroupsRead}. Failed Bunches: {reader?.TotalFailedBunches}. Failed Replicator: {reader?.TotalFailedReplicatorReceives} Null Exports: {reader?.NullHandles} Property Errors: {reader?.PropertyError} Failed Property Reads: {reader?.FailedToRead}");
-
-                    totalTime += sw.Elapsed.TotalMilliseconds;
-                    times.Add(sw.Elapsed.TotalMilliseconds);
-                }
-
-                Console.WriteLine();
+                totalTime += sw.Elapsed.TotalMilliseconds;
+                times.Add(sw.Elapsed.TotalMilliseconds);
             }
 
-            var fastest5 = times.OrderBy(x => x).Take(10);
-
-            Console.WriteLine($"Total Time: {totalTime}ms. Average: {((double)totalTime / count):0.00}ms");
-            Console.WriteLine($"Fastest 10 Time: {fastest5.Sum()}ms. Average: {(fastest5.Sum() / fastest5.Count()):0.00}ms");
-
-            Console.WriteLine("---- done ----");
-            Console.ReadLine();
+            Console.WriteLine();
         }
+
+        var fastest5 = times.OrderBy(x => x).Take(10);
+
+        Console.WriteLine($"Total Time: {totalTime}ms. Average: {((double)totalTime / count):0.00}ms");
+        Console.WriteLine($"Fastest 10 Time: {fastest5.Sum()}ms. Average: {(fastest5.Sum() / fastest5.Count()):0.00}ms");
+
+        Console.WriteLine("---- done ----");
+        Console.ReadLine();
     }
 }
