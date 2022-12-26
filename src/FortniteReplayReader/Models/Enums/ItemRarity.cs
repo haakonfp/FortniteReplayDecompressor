1cf0cf206ad71843134c8bcafe322c7b97ad6da5
haakonfp <haakonsnexus@gmail.com>
2022-12-26 21:43:33 +0100

    Update KillFeedEntry with enums from older seasons

--- src/FortniteReplayReader/Models/Enums/ItemRarity.cs
+++ src/FortniteReplayReader/Models/Enums/ItemRarity.cs
@@ -3,9 +3,14 @@
 public enum ItemRarity { Unknown, Common, Uncommon, Rare, Epic, Legendary, Mythic };
 public enum ItemType
 {
-	Unknown, Environment, Falling, PickAxe, AssaultRifle, BurstRifle, RocketLauncher, Pistol, PumpShotgun, TacticalShotgun, SMG, BoltSniper, Logout, Storm, Grenade, HeavyAR, Harpoon, Trap, RebootVan, SuppressedAR, SuppressedSniper, Minigun, C4, SentryTurret, SuppressedSMG,
-	MakeshiftAssaultRifle, MakeshiftSmg, MakeshiftShotgun, MakeshiftRevolver, MakeshiftBow,
-	PrimalAssaultRifle, PrimalSmg, PrimalShotgun, PrimalRevolver, PrimalBow, StinkBow,
-	FlameBow, MechBow, MechExplosiveBow, MetalBow,
-	Recycler, Shark, Raptor, Wolf
+    Unknown, Environment, Falling, PickAxe, AssaultRifle, BurstRifle, RocketLauncher, Pistol,
+    PumpShotgun, TacticalShotgun, SMG, BoltSniper, Logout, Storm, Grenade, HeavyAR, Harpoon,
+    Trap, RebootVan, SuppressedAR, SuppressedSniper, Minigun, C4, SentryTurret, SuppressedSMG,
+    MakeshiftAssaultRifle, MakeshiftSmg, MakeshiftShotgun, MakeshiftRevolver, MakeshiftBow,
+    PrimalAssaultRifle, PrimalSmg, PrimalShotgun, PrimalRevolver, PrimalBow, StinkBow,
+    FlameBow, MechBow, MechExplosiveBow, MetalBow, Recycler, Shark, Raptor, Wolf, LeverShotgun,
+    TrackerPistol, ShockwaveBow, GrapplerBow, Fire, DubShotgun, EggLauncher,
+    EnvironmentExplosion, EnvironmentExplosiveGas, Boat, InstantDeath, InstantEnvironment,
+    InstantEnvironmentUnderLandscape, SlonePulseRifle, ReconScanner, RailGun, KymeraRayGun,
+    Lightsaber, E11BlasterRifle, BuildingProp, Ufo, Ability
 };
